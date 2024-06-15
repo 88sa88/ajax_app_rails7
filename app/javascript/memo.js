b370ee928598179da174fa83f3df3fc66f3aa5db
88sa88 <yutooo.rikooo7@gmail.com>
2024-06-15 13:33:03 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,13 +1,13 @@
-function post(){
-  const form =document.getElementById("form");
-  form.addEventListener("submit",(e) => {
+function post (){
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const formDate = new FormDate(form);
+    const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
-    XHR.responseType = "json"
-    XHR.send(formDate);
- });
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
 };
 
 window.addEventListener('turbo:load', post);
